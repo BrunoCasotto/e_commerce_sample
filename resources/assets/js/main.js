18cd48d58cc16892e1fe39d2cadc31276f0bc072
Bruno Casotto <casottoalves@hotmail.com>
2017-06-11 12:48:57 -0300

    eslint config include on webpack

---
+++ resources/assets/js/main.js
@@ -0,0 +1,9 @@
+window.$ = window.jQuery = require('jquery')
+import "es6-promise"
+
+// ==== modules important ========
+import Vue from 'vue'
+
+new Vue({
+  el: '#app'
+})
--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,15 @@ module.exports = {
         }
       },
       {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+        options: {
+          emitError: true,
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
